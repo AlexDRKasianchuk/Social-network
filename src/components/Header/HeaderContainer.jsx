f952a8f0440d8582ecea7899f43987dc1df7bcd9
AlexDRKasianchuk <66167921+AlexDRKasianchuk@users.noreply.github.com>
2020-11-09 17:30:27 +0200

    login

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,29 @@
+import {setUserData} from '../../redux/authReducer'
+import React from 'react';
+import Header from './Header';
+import * as axios from 'axios';
+import { connect } from 'react-redux';
+
+class HeaderContainer extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true}).then(response => {
+      if (response.data.resultCode === 0) {
+        let { id, login, email } = response.data.data;
+        this.props.setUserData(id, email, login)
+      }
+    });
+
+  }
+
+  render() {
+    return <Header {...this.props} />
+  }
+}
+
+let mapStateToProps = (state) => ({
+  login: state.auth.login,
+  isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps,{setUserData})(HeaderContainer);
