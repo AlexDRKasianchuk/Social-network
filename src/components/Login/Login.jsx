bb0af0a862f43effe73f001783db91461955643d
AlexDRKasianchuk <66167921+AlexDRKasianchuk@users.noreply.github.com>
2020-11-14 17:28:17 +0200

    login form without axios

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-let Login = (props) => {
-   
-return <h1>Login</h1>
 
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+        <label htmlFor="Login">Login </label>
+            <Field name={"login"} placeholder={"Login"} component={'input'} />
+        </div>
+        <div>
+            
+        <label htmlFor="Password">Password </label>
+            <Field placeholder={"Password"} name={"password"} component={'input'} />
+        </div>
+        <div>
+            <Field type={"checkbox"} name={"rememberMe"} component={'input'} /> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
 }
 
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
+
+const Login = (props) => {
+   let onSubmit = (dataForm) =>{
+       console.log(dataForm);
+   }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+
+}
 export default Login;
